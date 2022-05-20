4c5fb47ce9bb45671464cb8e29e62f6e1ecd67cf
Keo5000 <104718816+Keo5000@users.noreply.github.com>
2022-05-20 12:24:20 +0200

    Accounting for difference in cases
    -Added .lower() to account for upper and lower case

--- task_010.py
+++ task_010.py
@@ -1,8 +1,10 @@
-string_1 = input("word 1:")
-string_2 = input("word 2:")
+string_1 = input()
+string_2 = input()
 
 
 def find_common_characters(string_1, string_2):
+    string_1 = string_1.lower()
+    string_2 = string_2.lower()
     result = ""
     for letter in string_1:
         if letter in string_2:
