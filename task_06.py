49c8fee844cd4e149d55f09caedad40c9580a1f8
Keo5000 <104718816+Keo5000@users.noreply.github.com>
2022-05-20 15:19:00 +0200

    Removed user input

--- task_06.py
+++ task_06.py
@@ -1,7 +1,3 @@
-value_1 = int(input())
-value_2 = int(input())
-value_3 = int(input())
-
 def maximum(value_1, value_2, value_3):
     if (value_1 > value_2) and (value_1 > value_3):
         largest = value_1
@@ -12,4 +8,4 @@ def maximum(value_1, value_2, value_3):
     return largest
 
 
-print(maximum(value_1, value_2, value_3))
+print(maximum(1, 2, 3))
