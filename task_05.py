9da724bd459e4b78cb5e1e2566f5e96eb010a530
Keo5000 <104718816+Keo5000@users.noreply.github.com>
2022-05-20 12:15:23 +0200

    input and variables
    Removed user input request and gave variables meaningful names.

--- task_05.py
+++ task_05.py
@@ -1,10 +1,14 @@
-a = int(input("side 1:"))
-b = int(input("side 2:"))
-c = int(input("side 3:"))
+side_1 = int(input())
+side_2 = int(input())
+side_3 = int(input())
 
 
-def area_of_triangle(a, b, c):  # where a, b and c are the sides of the triangle)
-    s = (a + b + c) / 2  # semi-parameter
-    area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+def area_of_triangle(side_1, side_2, side_3):
+    semi_parameter = (side_1 + side_2 + side_3) / 2
+    area = (
+        semi_parameter
+        * (semi_parameter - side_1)
+        * (semi_parameter - side_2)
+        * (semi_parameter - side_3)
+    ) ** 0.5
     return area
-
