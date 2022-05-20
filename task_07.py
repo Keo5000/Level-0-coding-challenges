a803db7a8bf19b477c58a9cd9ace82961df2ad59
Keo5000 <104718816+Keo5000@users.noreply.github.com>
2022-05-20 12:22:09 +0200

    User input removal, remove comments, print answer
    -Removed user input text
    -Removed comments
    -Printed answer

--- task_07.py
+++ task_07.py
@@ -1,14 +1,18 @@
-x = input("degrees celcius:")
+celsius = int(input())
+fahrenheit = int(input())
 
 
-def celsius_to_fahrenheit(x):  # where x is degrees celcius
-    temp_in_fahrenheit = (x * 9 / 5) + 32
+def celsius_to_fahrenheit(celsius):
+    temp_in_fahrenheit = (celsius * 9 / 5) + 32
     return temp_in_fahrenheit
 
 
-y = input("degrees fahrenheit:")
+print(celsius_to_fahrenheit(celsius))
 
 
-def fahrenheit_to_celsius(y):
-    temp_in_celsius = (y - 32) * 5 / 9  # where y is degrees fahrenheit
+def fahrenheit_to_celsius(fahrenheit):
+    temp_in_celsius = (fahrenheit - 32) * 5 / 9
     return temp_in_celsius
+
+
+print(fahrenheit_to_celsius(fahrenheit))
