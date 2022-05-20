4411330fde932dad036f25f661028650bfe4afbc
Keo5000 <104718816+Keo5000@users.noreply.github.com>
2022-05-20 15:17:09 +0200

    Removed user input,
    Changed semi_parameter  to semi_perimeter

--- task_05.py
+++ task_05.py
@@ -1,10 +1,5 @@
-side_1 = int(input())
-side_2 = int(input())
-side_3 = int(input())
-
-
 def area_of_triangle(side_1, side_2, side_3):
-    semi_parameter = (side_1 + side_2 + side_3) / 2
+    semi_perimeter = (side_1 + side_2 + side_3) / 2
     area = (
         semi_parameter
         * (semi_parameter - side_1)
