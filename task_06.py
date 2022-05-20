20e4940f2e93f6ffd22920daa11153243ef07234
Keo5000 <104718816+Keo5000@users.noreply.github.com>
2022-05-20 12:19:21 +0200

    Print() insertion, variables names
    -Printed the answer
    -Changed variables so that it is not the same as function

--- task_06.py
+++ task_06.py
@@ -1,8 +1,15 @@
-def maximum(a, b, c):
-    if (a > b) and (a > c):
-        max = a
-    elif (b > a) and (b > c):
-        max = b
-    elif (c > a) and (c > b):
-        max = c
-    return max
+value_1 = int(input())
+value_2 = int(input())
+value_3 = int(input())
+
+def maximum(value_1, value_2, value_3):
+    if (value_1 > value_2) and (value_1 > value_3):
+        largest = value_1
+    elif (value_2 > value_1) and (value_2 > value_3):
+        largest = value_2
+    elif (value_3 > value_1) and (value_3 > value_2):
+        largest = value_3
+    return largest
+
+
+print(maximum(value_1, value_2, value_3))
