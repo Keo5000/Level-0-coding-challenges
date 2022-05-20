ecf7fbfe96ef1c8f2d689c4c05510284c681a901
Keo5000 <104718816+Keo5000@users.noreply.github.com>
2022-05-20 15:22:19 +0200

    Simplified code

--- task_08.py
+++ task_08.py
@@ -1,27 +1,14 @@
-num = int(input("enter minutues:"))
-
-
 def convert_num_to_hours_and_minutes(num):
     hour = num // 60
     minutes = num % 60
-    if (hour > 1) and (minutes > 1):
+    if ((hour > 1) or (hour == 0)) and ((minutes > 1) or (minutes == 0)):
         print(hour, "hours" ",", minutes, "minutes")
-    elif (hour > 1) and (minutes == 1):
+    elif ((hour == 0) or (hour > 1)) and (minutes == 1):
         print(hour, "hours" ",", minutes, "minute")
-    elif (hour > 1) and (minutes == 0):
-        print(hour, "hours" ",", minutes, "minutes")
-    elif (hour == 0) and (minutes > 1):
-        print(hour, "hours" ",", minutes, "minutes") 
-    elif (hour == 0) and (minutes == 0):
-        print(hour, "hours" ",", minutes, "minutes")  
-    elif (hour == 0) and (minutes == 1):
-        print(hour, "hours" ",", minutes, "minute")
-    elif (hour <= 1) and (minutes > 1):
+    elif (hour == 1) and ((minutes == 0) or (minutes > 1)):
         print(hour, "hour" ",", minutes, "minutes")
-    elif (hour <= 1) and (minutes == 1):
-        print(hour, "hour" ",", minutes, "minute")
     else:
-        print(hour, "hour" ",", minutes, "minutes")
+        print(hour, "hour" ",", minutes, "minute")
 
 
-convert_num_to_hours_and_minutes(num)
+convert_num_to_hours_and_minutes(71)
