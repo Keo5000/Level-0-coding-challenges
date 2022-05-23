c5a200624a4274750c5d3b672597556457ff2555
Keo5000 <104718816+Keo5000@users.noreply.github.com>
2022-05-23 09:39:58 +0200

    Logic update
    Changed logic to >=

--- task_06.py
+++ task_06.py
@@ -1,11 +1,11 @@
 def maximum(value_1, value_2, value_3):
-    if (value_1 > value_2) and (value_1 > value_3):
+    if (value_1 >= value_2) and (value_1 >= value_3):
         largest = value_1
-    elif (value_2 > value_1) and (value_2 > value_3):
+    elif (value_2 >= value_1) and (value_2 >= value_3):
         largest = value_2
-    elif (value_3 > value_1) and (value_3 > value_2):
+    elif (value_3 >= value_1) and (value_3 >= value_2):
         largest = value_3
     return largest
 
 
-print(maximum(1, 2, 3))
+print(maximum(1, 1, 0))
