c708cac8ffd7c81ab1be97ffb062fda79689f73d
Keo5000 <104718816+Keo5000@users.noreply.github.com>
2022-05-23 16:40:05 +0200

    Removed duplicates in output
    Added 'continue' to skip duplicates.

--- task_010.py
+++ task_010.py
@@ -4,8 +4,11 @@ def find_common_characters(string_1, string_2):
     result = ""
     for letter in string_2:
         if letter in string_1:
+            if letter in result:
+                continue
             result += letter
+
     return "Common letters: " + ", ".join(result)
 
 
-print(find_common_characters("HOUSES", "computers"))
+print(find_common_characters("HOUSES", "computerss"))
