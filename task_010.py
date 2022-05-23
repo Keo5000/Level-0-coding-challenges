244ab24f1deebcbb7854387e266f5fa524067050
Keo5000 <104718816+Keo5000@users.noreply.github.com>
2022-05-23 14:58:01 +0200

    Updated logic
    Removed duplicates from output.

--- task_010.py
+++ task_010.py
@@ -2,10 +2,10 @@ def find_common_characters(string_1, string_2):
     string_1 = string_1.lower()
     string_2 = string_2.lower()
     result = ""
-    for letter in string_1:
-        if letter in string_2:
+    for letter in string_2:
+        if letter in string_1:
             result += letter
     return "Common letters: " + ", ".join(result)
 
 
-print(find_common_characters("HOUSE", "computers"))
+print(find_common_characters("HOUSES", "computers"))
