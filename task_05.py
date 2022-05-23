665bf62d74bbe9aca60da42f46d1d09a89ad92e0
Keo5000 <104718816+Keo5000@users.noreply.github.com>
2022-05-23 08:09:32 +0200

    Changed variable name

--- task_05.py
+++ task_05.py
@@ -1,11 +1,12 @@
 def area_of_triangle(side_1, side_2, side_3):
     semi_perimeter = (side_1 + side_2 + side_3) / 2
     area = (
-        semi_parameter
-        * (semi_parameter - side_1)
-        * (semi_parameter - side_2)
-        * (semi_parameter - side_3)
+        semi_perimeter
+        * (semi_perimeter - side_1)
+        * (semi_perimeter - side_2)
+        * (semi_perimeter - side_3)
     ) ** 0.5
     return area
+
 
 print(area_of_triangle(3, 4, 5))
